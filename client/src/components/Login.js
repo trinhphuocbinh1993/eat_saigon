4f03a644c212a86e83bc1bfd66d96dd0a04bd4dd
trinhphuocbinh1993 <37438525+trinhphuocbinh1993@users.noreply.github.com>
2019-11-12 11:38:14 +0700

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button, Form, Container } from 'react-bootstrap'
 
 class Login extends React.Component {
     constructor() {
@@ -10,6 +11,8 @@ class Login extends React.Component {
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.emailError = this.emailError.bind(this)
+        this.passwordError = this.passwordError.bind(this)
     }
 
     handleChange(event) {
@@ -18,34 +21,93 @@ class Login extends React.Component {
             [name]: value
         })
     }
+
+    emailError(event) {
+
+        event.preventDefault();
+        //valid email
+        if (!this.state.email) {
+            this.setState({
+                emailErr: "Type email please"
+            })
+        } else {
+            this.setState({
+                emailErr: ""
+            })
+        }
+    }
+    passwordError(event) {
+
+        event.preventDefault();
+        //valid password
+        if (!this.state.password) {
+            this.setState({
+                passwordErr: "Type password please"
+            })
+        } else {
+            this.setState({
+                passwordErr: ""
+            })
+        }
+    }
+
     handleSubmit(event) {
         event.preventDefault();
-        const data = this.state;
 
-        fetch('http://localhost:3002/api/users/login', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        }).then((response) => response.json())
-        .then((responseJson) => {
-        alert(responseJson.message);
-        if(responseJson.token){
-            alert(responseJson.token)
-            localStorage.setItem('user', responseJson.token);
-            localStorage.setItem('userid', responseJson.userid);
-            this.props.history.push("/admin");
+        if ((this.state.emailErr === "") && (this.state.passwordErr === "")) {
+            const data = this.state;
+
+            fetch('http://localhost:3002/api/users/login', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            }).then((response) => response.json())
+                .then((responseJson) => {
+                    alert(responseJson.message);
+                    if (responseJson.token) {
+                        alert(responseJson.token)
+                        localStorage.setItem('user', responseJson.token);
+                        localStorage.setItem('userid', responseJson.userid);
+                        this.props.history.push("/admin");
+                    }
+                })
         }
-        })
     }
 
     render() {
         return (
             <div>
                 <div>{this.state.errors}</div>
-                <form onSubmit={this.handleSubmit}>
+                <Container>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control name="email" type="email" placeholder="Enter email" value={this.state.email} onChange={this.handleChange} onBlur={this.emailError} />
+                            <Form.Text className="text-muted">
+                                {this.state.emailErr ? this.state.emailErr : ""}
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control name="password" type="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} onBlur={this.passwordError} />
+                        </Form.Group>
+                        <Form.Text className="text-muted">
+                            {this.state.passwordErr ? this.state.passwordErr : ""}
+                        </Form.Text>
+                        <br/>
+                        <Button variant="primary" type="submit">
+                            Submit
+  </Button>
+                    </Form>
+                </Container>
+
+
+
+                {/* <form onSubmit={this.handleSubmit}>
 
                     <label htmlFor="email">Email</label>
                     <br />
@@ -55,8 +117,8 @@ class Login extends React.Component {
                     <br />
                     <input name="password" id="password" type="password" value={this.state.password} onChange={this.handleChange} />
                     <br />
-                    <button type="submit">Submit</button>
-                </form>
+                    <Button type="submit" variant="primary">Submit</Button>
+                </form> */}
 
                 <h1>email: {this.state.email}</h1>
                 <h1>password: {this.state.password}</h1>
